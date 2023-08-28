8cbab4c066d900c1ea7c419b6ea2f941deb5a04a
Pilla Vineeth Sai Ram Kumar <vineeth.p@deccansoft.net>
2023-08-28 21:48:21 +0530

    Add project files.

---
+++ ImageUploadInWWWRootFolder/Controllers/FileUploadApiController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageUploadInWWWRootFolder.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadApiController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public FileUploadApiController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+        [HttpPost]
+        public async Task<ActionResult>UploadImage(IFormFile file)
+        {
+            try
+            {
+                string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "uploads");
+                Directory.CreateDirectory(uploadsFolder); // Ensure 'uploads' folder exists
+
+
+
+                string uniqueFileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+
+
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(fileStream);
+                }
+
+                return Ok(file);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
